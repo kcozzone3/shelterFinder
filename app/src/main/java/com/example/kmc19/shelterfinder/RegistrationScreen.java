7a661f59205e8adf217056cf1528339b6db394b7
Harry Wang <harrywang703@gmail.com>
2018-02-24 18:04:10 -0500

    Reorganized files?

--- app/src/main/java/com/example/kmc19/shelterfinder/RegistrationScreen.java
+++ app/src/main/java/com/example/kmc19/shelterfinder/RegistrationScreen.java
@@ -3,71 +3,46 @@ package com.example.kmc19.shelterfinder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Spinner;
 
-/**
- * The registration screen in which users can create accounts to be stored in the database.
- *
- * @author hwang703
- */
+
 public class RegistrationScreen extends AppCompatActivity {
+    EditText editEmail, editUsername, editPassword;
+    private Spinner usertype;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_screen);
+        editEmail = (EditText)findViewById(R.id.registration_email_box);
+        editUsername = (EditText)findViewById(R.id.registration_name_box);
+        editPassword = (EditText)findViewById(R.id.registration_password_box);
+        usertype = (Spinner)findViewById(R.id.user_type_spinner);
 
-        Button registerButton = findViewById(R.id.registration_register_button);
-        Button cancelButton = findViewById(R.id.registration_cancel_button);
+        ArrayAdapter<String> adapterCS = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, UserType.values());
+        adapterCS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        usertype.setAdapter(adapterCS);
 
-        final TextView usernameError = findViewById(R.id.registration_email_error_message);
-        final TextView fullNameError = findViewById(R.id.registration_name_error_message);
-        final TextView passwordError = findViewById(R.id.registration_password_error_message);
 
-        final EditText username = findViewById(R.id.registration_email_box);
-        final EditText fullName = findViewById(R.id.registration_name_box);
-        final EditText password = findViewById(R.id.registration_password_box);
-
-        final Model_UserList userList = new Model_UserList();
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String id = username.getText().toString();
-                String name = fullName.getText().toString();
-                String pass = password.getText().toString();
-                boolean invalidUser = !(id.matches(".*@.*"));
-                boolean invalidPass = !(pass.matches(".*[A-Za-z].*") && pass.matches(".*[0-9].*"));
-                boolean invalidName = !name.matches("[A-Za-z]*\\s[A-Za-z]*");
-                if (invalidUser) {
-                    usernameError.setVisibility(View.VISIBLE);
-                }
-                if (invalidName) {
-                    fullNameError.setVisibility(View.VISIBLE);
-                }
-                if (invalidPass) {
-                    passwordError.setVisibility(View.VISIBLE);
-                }
-                if (!invalidUser && !invalidPass && !invalidName) {
-                    Model_User newUser = new Model_User(id, name, pass, false);
-                    userList.addUser(newUser);
-                    Intent intent = new Intent(getBaseContext(), ShelterList.class);
-                    finish();
-                    startActivity(intent);
-                }
-            }
-        });
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), HomeScreen.class);
-                finish();
-                startActivity(intent);
-            }
-        });
     }
+
+    public void OnRegister (View view) {
+        String username = editUsername.getText().toString();
+        String password = editPassword.getText().toString();
+        String email = editEmail.getText().toString();
+        String userTypeStr = usertype.getSelectedItem().toString();
+        String type = "register";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, email, username, password, userTypeStr);
+    }
+    public void OnCancel(View view) {
+        startActivity(new Intent(this, HomeScreen.class));
+    }
+
+
 }
+
+
