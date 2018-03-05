836a21c07cbf2a4c6d32e933e621ba7958a13ef1
Harry Wang <harrywang703@gmail.com>
2018-03-04 21:10:36 -0500

    Database functionality broadened

--- app/src/main/java/com/example/kmc19/shelterfinder/ShelterList.java
+++ app/src/main/java/com/example/kmc19/shelterfinder/ShelterList.java
@@ -1,37 +1,36 @@
 package com.example.kmc19.shelterfinder;
 
 import android.content.Intent;
-import android.renderscript.ScriptGroup;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShelterList extends AppCompatActivity {
-    private ListView shelterView;
     private List<ShelterInfo> shelterList = new ArrayList<>();
+    ListView shelterView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shelter_list);
+
         shelterView = findViewById(R.id.shelter_list_view);
-        shelterList = new ArrayList<>();
-        readShelterdata();
-        final ArrayAdapter<ShelterInfo> arrayAdapter = new ArrayAdapter<ShelterInfo>(this,
-                android.R.layout.simple_list_item_1, shelterList);
-        shelterView.setAdapter(arrayAdapter);
+        getJSON("http://128.61.112.83:8888/retrieve_data.php");
         shelterView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -49,70 +48,72 @@ public class ShelterList extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
-    private void readShelterdata() {
-        InputStream is = getResources().openRawResource(R.raw.shelter);
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(is, Charset.forName("UTF-8"))
-        );
-
-        String line ="";
-        try {
-            //step over headers
-            reader.readLine();
-            while((line = reader.readLine()) != null) {
-                //Split by ","
-                String[] tokens = line.split(",");
-                //Read data
-                ShelterInfo shelterInfo= new ShelterInfo();
-                shelterInfo.setShelterName(tokens[1]);
-                if(tokens[2].equals("")){
-                    shelterInfo.setCapacity("N/A");
-                }else{
-                    shelterInfo.setCapacity(tokens[2]);
-                }
-                shelterInfo.setRestrictions(tokens[3]);
-                shelterInfo.setLongitude(tokens[4]);
-                shelterInfo.setLatitude(tokens[5]);
-                String stringBuffer = "";
-                int n = 6;
-                for (int i = 0; i < 2; i++) {
-                    if (tokens[n].charAt(0) == '\"') {
-                        boolean endOfBuffer = false;
-                        while (!endOfBuffer) {
-                            if (tokens[n].charAt(tokens[n].length() - 1) == '\"') {
-                                tokens[n] = tokens[n].substring(0, tokens[n].length() - 1);
-                                stringBuffer += tokens[n];
-                                endOfBuffer = true;
-                            } else {
-                                stringBuffer += tokens[n].substring(1) + ", ";
-                            }
-                            n++;
-                        }
-                    } else {
-                        stringBuffer += tokens[n];
-                        n++;
-                    }
-                    if (i == 0) {
-                        shelterInfo.setAddress(stringBuffer);
-                    } else {
-                        shelterInfo.setSpecialNotes(stringBuffer);
-                    }
-                    stringBuffer = "";
-                }
-                shelterInfo.setPhone(tokens[n]);
-                System.out.println("Success");
-                shelterList.add(shelterInfo);
-
-                Log.d("ShelterList", "Just created" + shelterInfo);
 
 
+    private void getJSON(final String urlWebService) {
+
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
             }
-        } catch (IOException e) {
-            Log.wtf("ShelterList", "Error reading shelter.csv" + line, e);
-            e.printStackTrace();
-        }
 
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                // Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+
+    private void loadIntoListView(String json) throws JSONException {
+        JSONArray jsonArray = new JSONArray(json);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+            ShelterInfo shelterInfo = new ShelterInfo();
+            shelterInfo.setShelterName(obj.getString("shelter_name"));
+            if(obj.getString("capacity").equals("")){
+                shelterInfo.setCapacity("N/A");
+            }else{
+                shelterInfo.setCapacity(obj.getString("capacity"));
+            }
+            shelterInfo.setRestrictions(obj.getString("restrictions"));
+            shelterInfo.setLongitude(obj.getString("longitude"));
+            shelterInfo.setLatitude(obj.getString("latitude"));
+            shelterInfo.setAddress(obj.getString("address"));
+            shelterInfo.setSpecialNotes(obj.getString("special_notes"));
+            shelterInfo.setPhone(obj.getString("phone_number"));
+            shelterList.add(shelterInfo);
+
+        }
+        ArrayAdapter<ShelterInfo> arrayAdapter = new ArrayAdapter<ShelterInfo>(this, android.R.layout.simple_list_item_1, shelterList);
+        shelterView.setAdapter(arrayAdapter);
     }
 }
