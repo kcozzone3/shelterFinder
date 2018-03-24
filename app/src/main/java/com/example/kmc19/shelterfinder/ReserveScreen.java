de909c7412ba27c83812a9111e52677b695c4d21
Harry Wang <harrywang703@gmail.com>
2018-03-24 10:28:14 -0500

    Added UI for reservation

---
+++ app/src/main/java/com/example/kmc19/shelterfinder/ReserveScreen.java
@@ -0,0 +1,14 @@
+package com.example.kmc19.shelterfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReserveScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_screen);
+    }
+}
+
