58e5eb8895087859843dca5dcbbb96c5003b00bd
JacksonFan1225 <jacksonfan951225@gmail.com>
2018-04-07 17:46:12 -0400

    add project default xml

---
+++ shelterFinder/app/src/main/java/com/example/kmc19/shelterfinder/OnSwipeListener.java
@@ -0,0 +1,80 @@
+package com.example.kmc19.shelterfinder;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Toast;
+
+public class OnSwipeListener implements OnTouchListener {
+
+    private final GestureDetector gestureDetector;
+    private Context context;
+
+    public boolean onTouch(final View view, final MotionEvent motionEvent) {
+        return gestureDetector.onTouchEvent(motionEvent);
+    }
+
+    public GestureDetector getGestureDetector() {
+        return gestureDetector;
+    }
+
+    public OnSwipeListener(Context context) {
+        super();
+        this.context = context;
+        gestureDetector = new GestureDetector(context, new GestureListener());
+    }
+
+    private final class GestureListener extends SimpleOnGestureListener {
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            try {
+                float diffX = e2.getRawX() - e1.getRawX();
+                float diffY = e2.getRawY() - e1.getRawY();
+                if ((Math.abs(diffX) - Math.abs(diffY)) > SWIPE_THRESHOLD) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
+                        }
+                        result = true;
+                    }
+                } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom();
+                    } else {
+                        onSwipeTop();
+                    }
+                }
+            } catch (Exception e) {
+                int duration = Toast.LENGTH_LONG;
+                Toast.makeText(context, e.getMessage(), duration).show();
+            }
+            return result;
+        }
+    }
+
+    public void onSwipeRight() {
+    }
+
+    public void onSwipeLeft() {
+    }
+
+    public void onSwipeBottom() {
+    }
+
+    public void onSwipeTop() {
+    }
+}
---
+++ shelterFinder/app/src/main/java/com/example/kmc19/shelterfinder/ReserveScreen.java
@@ -0,0 +1,60 @@
+package com.example.kmc19.shelterfinder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class ReserveScreen extends AppCompatActivity {
+    private Spinner reservation;
+    private String sheltername;
+
+    //Need to get Extra
+    private String email, capacity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_screen);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        //Set number of beds one can reserve
+        Spinner mspin = findViewById(R.id.reserve_number_spinner);
+
+        //get capcity
+        capacity = extras.getString("capacity");
+        int CAP = Integer.parseInt(capacity);
+        Integer[] items = new Integer[CAP];
+        for (int a = 0; a < items.length; a++) {
+            items[a] = (a + 1);
+        }
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, items);
+        mspin.setAdapter(adapter);
+
+        //get the number of beds reserved
+        reservation = findViewById(R.id.reserve_number_spinner);
+
+        //get the sheltername
+;
+        sheltername = extras.getString("shelterName");
+
+        //get email
+        email = extras.getString("email");
+    }
+
+    public void OnReserve(View view) {
+        String numSpots = reservation.getSelectedItem().toString();
+        BackgroundCheck backgroundCheck = new BackgroundCheck(this, email, sheltername, numSpots);
+        backgroundCheck.execute(email);
+    }
+    public void OnCancel(View view) {
+        Intent intent = new Intent(getBaseContext(), ShelterList.class);
+        finish();
+        startActivity(intent);
+    }
+}
+
+
