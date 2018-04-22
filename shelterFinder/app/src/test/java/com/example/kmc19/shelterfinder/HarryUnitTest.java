c4da109b35c445ca39f7c5099863ce6a15e5a752
Harry Wang <harrywang703@gmail.com>
2018-04-22 13:12:43 -0400

    Harry's unit tests added

--- shelterFinder/app/src/test/java/com/example/kmc19/shelterfinder/HarryUnitTest.java
+++ shelterFinder/app/src/test/java/com/example/kmc19/shelterfinder/HarryUnitTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Tests ShelterInfo class's special notes getter and setter methods.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
